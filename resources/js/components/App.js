17d2b1b35d24400bfa775f69cc987fb101249c8e
datikken <tikken23@gmail.com>
2021-03-13 16:21:38 +0300

    apollo client graphql implemented

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from "react-router-dom";
-import { Route, Switch} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import {Home} from '../pages/Home';
 import {Signin} from '../pages/Signin'
 import {Signup} from '../pages/Signup'
 import {createBrowserHistory} from "history";
+import {ApolloClient, InMemoryCache} from '@apollo/client';
+import {gql} from '@apollo/client';
 
 const history = createBrowserHistory();
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache()
+});
+
+client
+.query({
+    query: gql`
+        query articles {
+            articles {
+                title,
+                author {
+                    email
+                }
+            }
+        }
+    `
+})
+.then(result => console.log(result));
 
 function App() {
     return (
