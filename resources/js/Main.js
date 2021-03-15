ad764754c425af2a26fadf9cef14394d6772059e
datikken <tikken23@gmail.com>
2021-03-16 00:05:48 +0300

    all articles list laravel nova

--- resources/js/Main.js
+++ resources/js/Main.js
@@ -6,18 +6,17 @@ import {BrowserRouter as Router} from "react-router-dom";
 import {ApolloClient, InMemoryCache} from '@apollo/client';
 import {ApolloProvider} from '@apollo/client';
 
-import {gql} from '@apollo/client';
-
 import {Signin} from './pages/Signin'
 import {Signup} from './pages/Signup'
 import {Boards} from './pages/Boards';
 import {Home} from './pages/Home';
 
 const history = createBrowserHistory();
+
 const client = new ApolloClient({
     uri: '/graphql',
     cache: new InMemoryCache()
-});
+})
 
 function Main() {
     return (
--- resources/js/pages/Home.js
+++ resources/js/pages/Home.js
@@ -1,18 +1,31 @@
 import React from "react";
 import Drawer from '../components/Drawer';
-import {TodoListView} from '../components/TodoListView';
-import TodoList from '../stores/TodoList'
-import Todo from '../stores/Todo';
+import Card from '../components/Card';
+import { gql, useQuery } from '@apollo/client';
 
-const store = new TodoList([new Todo("Get Coffee"), new Todo("Write simpler code")])
-
+const GEAT_ARTICLES = gql`
+    query GetArticles {
+        articles {
+            id,
+            title,
+            author {
+                name
+            }
+        }
+    }
+`;
 export function Home()
 {
+    const { loading, error, data } = useQuery(GEAT_ARTICLES);
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+
     return (
         <>
             <Drawer>
-                <h1>Motherfuck</h1>
-                <TodoListView todoList={store} />
+                {data.articles.map(article => (
+                    <Card article={article} key={article.id} />
+                ))}
             </Drawer>
         </>
     );
