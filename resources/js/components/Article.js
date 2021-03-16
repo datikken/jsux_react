edbc0d943ceb50fd4eb6018aae4e0e85e2a8832c
datikken <tikken23@gmail.com>
2021-03-16 22:31:11 +0300

    render articles

--- resources/js/components/Article.js
+++ resources/js/components/Article.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
+import {gql, useQuery} from "@apollo/client";
+import Spinner from "./Spinner";
 
-export function Article({ article }) {
-    console.log(article, 'mfka')
+export function Article({ id }) {
+    const GEAT_ARTICLE = gql`
+        query GetArticles {
+            article(id: ${id}) {
+                id,
+                title,
+                content,
+                author {
+                    name
+                }
+            }
+        }
+    `;
+
+    const {loading, error, data} = useQuery(GEAT_ARTICLE);
+
+    if (loading) return <Spinner/>;
+    if (error) return `Error! ${error.message}`;
 
     return (
         <>
-            <h1>{article.content}</h1>
+            { ReactHtmlParser (data.article.content) }
         </>
     );
 }
