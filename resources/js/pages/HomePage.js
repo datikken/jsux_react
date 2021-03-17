473ebeed1bfe948e2f923bc01c814f0b88de4936
datikken <tikken23@gmail.com>
2021-03-17 17:38:57 +0300

    testing

--- resources/js/pages/HomePage.js
+++ resources/js/pages/HomePage.js
@@ -6,6 +6,7 @@ export function HomePage()
     return (
         <>
             <Drawer>
+                <h1>Home page git actions test</h1>
                 <ArticlesList />
             </Drawer>
         </>
