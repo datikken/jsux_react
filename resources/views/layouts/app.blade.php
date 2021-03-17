eae1f06bcd820e237207c051bbd9ccb8e0396d30
datikken <tikken23@gmail.com>
2021-03-17 18:43:37 +0300

    favicons added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
     <link type="image/x-icon" rel="shortcut icon" href="/favicon/favicon.ico">
     <link type="image/png" sizes="16x16" rel="icon" href="/favicon/favicon-16x16.png">
     <link type="image/png" sizes="32x32" rel="icon" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" href="/favicon/favicon-192x192.png" sizes=192x192>
+    <link rel="icon" type="image/png" href="/favicon/favicon-512x512.png" sizes=512x512>
     <link sizes="16x16" rel="apple-touch-icon" href="/favicon/apple-touch-icon.png">
     <link sizes="32x32" rel="apple-touch-icon" href="/favicon/apple-touch-icon.png">
     <link rel="manifest" href="/favicon/site.webmanifest">
